d570e3a4538781f15b8f9642a491e96f7091c43d
niru9711 <nirusanant97@gmail.com>
2024-05-02 18:39:26 +0530

    post_API

---
+++ app/api/posts/route.js
@@ -0,0 +1,16 @@
+import connectMongo from "../../../db/connectMongo";
+import PostModel from "../../../models/postModel";
+
+
+export async function GET() {
+    try {
+        await connectMongo();
+        const postData = await PostModel.find({});
+        return Response.json(postData);
+    }
+    catch (error) {
+        return Response.json({ message: error.message });
+
+    }
+
+}
