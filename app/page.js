4445b8203c93132cbf0512988c9b8f1355bd76ba
niru9711 <nirusanant97@gmail.com>
2024-05-04 13:58:04 +0530

    pagination

--- app/page.js
+++ app/page.js
@@ -1,6 +1,7 @@
 'use client'
 import Link from "next/link";
 import { useEffect, useState, useRef } from "react";
+import Pagination from "@/components/Pagination";
 
 export default function Home() {
   const [posts, setPosts] = useState();
@@ -37,20 +38,9 @@ export default function Home() {
         <button onClick={searchPost} disabled={search} className="px-4 py-2 bg-blue-500 text-white rounded-md ml-4">{search? '...' : 'Search'}</button>
       </div>
 
-      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
-        {posts && posts.map((post) => (
-        <Link href={"/post/"+post._id}>
-        <div className="border border-gray-200 p-4">
-          <img className="object-fill w-full h-48 hover:object-scale-down mb-4" src={post.image} alt="Post Image" />
-          <h2 className="text-xl font-semibold mb-2">{post.title}</h2>
-          <p className="text-gray-600">{post.short_description}</p>
-        </div>
-        </Link>
-        )
-        )}
+      {posts && posts.length > 0 &&  <Pagination data={posts}/>}
 
-        {posts && !posts.length > 0 && inputRef.current.value && <p>No available posts for your searching: <b>{inputRef.current.value}</b></p>}
-      </div>
+      {posts && !posts.length > 0 && inputRef.current.value && <p>No available posts for your searching: <b>{inputRef.current.value}</b></p>}
     </>
   );
 }
---
+++ components/Pagination.js
@@ -0,0 +1,69 @@
+'use client';
+import { useState } from 'react';
+import Link from 'next/link';
+
+const PAGE_SIZE = 3; // Number of items per page
+
+function Pagination({ data }) {
+    const [currentPage, setCurrentPage] = useState(1);
+
+    const totalPages = Math.ceil(data.length / PAGE_SIZE);
+
+    // Calculate start and end index for the current page
+    const startIndex = (currentPage - 1) * PAGE_SIZE;
+    const endIndex = Math.min(startIndex + PAGE_SIZE, data.length);
+
+    // Get the data for the current page
+    const currentPageData = data.slice(startIndex, endIndex);
+
+    const goToPage = (pageNumber) => {
+        setCurrentPage(pageNumber);
+    };
+
+    return (
+        <>
+            <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
+                {currentPageData.map((item, index) => (
+                    <Link href={"/post/" + item._id}>
+                        <div className="border border-gray-200 p-4">
+                            <img className="object-fill w-full h-48 hover:object-scale-down mb-4" src={item.image} alt="Post Image" />
+                            <h2 className="text-xl font-semibold mb-2">{item.title}</h2>
+                            <p className="text-gray-600">{item.short_description}</p>
+                        </div>
+                    </Link>
+                ))}
+            </div>
+
+            <div className="flex justify-center mt-4 mb-4">
+                <button
+                    onClick={() => goToPage(currentPage - 1)}
+                    disabled={currentPage === 1}
+                    className="mr-2 px-3 py-1 bg-gray-200 hover:bg-gray-300 rounded"
+                >
+                    Previous
+                </button>
+                {Array.from({ length: totalPages }, (_, i) => (
+                    <button
+                        key={i}
+                        onClick={() => goToPage(i + 1)}
+                        className={`mr-2 px-3 py-1 ${currentPage === i + 1 ? 'bg-blue-500 text-white' : 'bg-gray-200 hover:bg-gray-300'
+                            } rounded`}
+                    >
+                        {i + 1}
+                    </button>
+                ))}
+                <button
+                    onClick={() => goToPage(currentPage + 1)}
+                    disabled={currentPage === totalPages}
+                    className="ml-2 px-3 py-1 bg-gray-200 hover:bg-gray-300 rounded"
+                >
+                    Next
+                </button>
+            </div>
+
+        </>
+
+    );
+}
+
+export default Pagination;
