4fe2aff243188c78c4e646ec6d61ea78cfa11fd8
niru9711 <nirusanant97@gmail.com>
2024-07-23 23:10:03 +0530

    add_login_navbar

--- components/Navbar.js
+++ components/Navbar.js
@@ -7,7 +7,7 @@ export default function Navbar() {
             <Link href="/"> <h1 className="text-2xl">Blog</h1> </Link>
             <nav className="space-x-4">
                 <Link href="/" className="text-blue-500">Home</Link>
-                <Link href="/about" className="text-blue-500">About</Link>
+                <Link href="/login" className="text-blue-500">Login</Link>
                 <Link href="/contact" className="text-blue-500">Contact</Link>
             </nav>
         </div>
