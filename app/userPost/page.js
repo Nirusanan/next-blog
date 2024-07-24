cff6798de63ab3af3e2ab5e41612629c37763121
niru9711 <nirusanant97@gmail.com>
2024-07-25 00:04:27 +0530

    userPostPage

---
+++ app/userPost/page.js
@@ -0,0 +1,13 @@
+import UserPost from "@/components/UserPost";
+
+export const metadata = {
+  title: "Blog | Posts",
+  description: "Post page",
+};
+
+export default function page() {
+  return (
+    <UserPost />
+  )
+  
+}
