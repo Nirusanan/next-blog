ee95732a6eee873e46573ccc3ffbef70510cbe9e
niru9711 <nirusanant97@gmail.com>
2024-07-28 23:52:03 +0530

    comments_model

---
+++ models/Comment.js
@@ -0,0 +1,25 @@
+import mongoose from 'mongoose';
+
+const CommentSchema = new mongoose.Schema({
+  comment: {
+    type: String,
+    required: true,
+  },
+  postId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Post',
+    required: true,
+  },
+  userId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+const Comment = mongoose.models.Comment || mongoose.model('Comment', CommentSchema);
+export default Comment;
