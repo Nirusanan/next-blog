7b82d8d201ad8ba8be763a73867b37f6038eff3b
niru9711 <nirusanant97@gmail.com>
2024-07-28 23:54:00 +0530

    create_post

---
+++ app/userPost/create/page.js
@@ -0,0 +1,13 @@
+import CreatePost from "@/components/CreatePost";
+
+export const metadata = {
+  title: "Blog | Posts",
+  description: "Post page",
+};
+
+export default function page() {
+  return (
+    <CreatePost />
+  )
+  
+}
