4d1390c858315b6a278d20a0e4f2ffb055b05577
niru9711 <nirusanant97@gmail.com>
2024-07-29 23:53:52 +0530

    view_specific_blogPost_likes_comments

--- app/post/[id]/page.js
+++ app/post/[id]/page.js
@@ -1,16 +1,18 @@
 import Post from '@/components/Post';
 
 async function getData(id) {
-    const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/post/${id}`);
+    const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/post/${id}?_=${Date.now()}`);
     if (!response.ok) {
         throw new Error('Failed to fetch data');
     }
     const json = await response.json();
+    console.log('Fetched data:',json);
     return json;
 }
 
 
 export default async function page({ params }) {
     const post = await getData(params.id);
+    console.log(post);
     return <Post post={post}/>
 }
