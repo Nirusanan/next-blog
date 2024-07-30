e11571cd4e68b18266ee8138460f0320bcf3ffd7
niru9711 <nirusanant97@gmail.com>
2024-07-30 22:04:26 +0530

    add_auther

--- app/api/post/[id]/route.js
+++ app/api/post/[id]/route.js
@@ -1,15 +1,22 @@
 import connectMongo from "@/db/connectMongo";
 import PostModel from "@/models/postModel";
 import { NextResponse } from "next/server";
+import userModel from "@/models/userModel";
 
 
 
 export async function GET(req, {params}) {
     try {
         await connectMongo();
-        const post = await PostModel.findById(params.id);
+        // const post = await PostModel.findById(params.id);
+        const post = await PostModel.findById(params.id).populate({
+            path: 'userId',
+            model: userModel,
+            select: 'name',
+          })
+          .exec();
         console.log('Post likes:',post.likes); 
-
+        
         return Response.json(post);
     }
     catch (error) {
